6ddf5c11b6a1637214d178fe4416472fe190f695
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-04-28 14:53:14 +0200

    Added customer list

---
+++ src/RealWorld/Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿namespace RealWorld.Models;
+
+
+public abstract class Base
+{
+
+}
+
+public abstract class EntityBase : Base
+{
+    public int Id { get; set; }
+}
+
+public class Customer : EntityBase
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+}
