be5b77ef42978131a34e31605e52b0af121289b9
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-04-28 16:02:57 +0200

    Refactor: Use named HttpClient

--- src/RealWorld/Program.cs
+++ src/RealWorld/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using RealWorld;
+using RealWorld.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,5 +10,10 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 var baseUrl = "https://localhost:7248";
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseUrl) });
+
+builder.Services.AddHttpClient<ICustomerService, ApiCustomerService>(client =>
+{
+    client.BaseAddress = new Uri(baseUrl);
+});
 
 await builder.Build().RunAsync();
---
+++ src/RealWorld/Services/ApiCustomerService.cs
@@ -0,0 +1,26 @@
+﻿using RealWorld.Models;
+using System.Net.Http.Json;
+
+namespace RealWorld.Services;
+
+// Abstract
+public interface ICustomerService
+{
+    Task<List<Customer>> GetAllAsync();
+}
+
+// Concrete
+public class ApiCustomerService : ICustomerService
+{
+    private HttpClient Client;
+
+    public ApiCustomerService(HttpClient client)
+    {
+        Client = client;
+    }
+
+    public Task<List<Customer>> GetAllAsync()
+    {
+        return Client.GetFromJsonAsync<List<Customer>>("api/customers");
+    }
+}
