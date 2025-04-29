7b16241320a8e80b741682f2b0a01fd3a3363122
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-04-29 16:09:42 +0200

    Added complex Cascading Parameter

---
+++ src/RealWorld/Models/Margin.cs
@@ -0,0 +1,9 @@
+﻿namespace RealWorld.Models;
+
+public class Margins
+{
+    public decimal? Top { get; set; }
+    public decimal? Right { get; set; }
+    public decimal? Bottom { get; set; }
+    public decimal? Left { get; set; }
+}
