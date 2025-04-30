025dd629209ca153ff449de411567c6f2d2dcc60
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-04-30 17:28:13 +0200

    Added call js from blazor

--- src/RealWorld/Authorization/JwtAuthenticationStateProvider.cs
+++ src/RealWorld/Authorization/JwtAuthenticationStateProvider.cs
@@ -1,15 +1,22 @@
 ﻿using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.JSInterop;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 
+
 namespace RealWorld.Authorization;
 
-public class JwtAuthenticationStateProvider(ILocalStorageService localStorage) : AuthenticationStateProvider
+public class JwtAuthenticationStateProvider(ILocalStorageService localStorage, IJSRuntime JS) : AuthenticationStateProvider
 {
     public override async Task<AuthenticationState> GetAuthenticationStateAsync()
     {
         var token = await localStorage.GetItemAsStringAsync("access_token");
+
+        await JS.InvokeVoidAsync("console.log", "Token: " + token);
+
+        var value = await JS.InvokeAsync<string>("localStorage.getItem", "a");
+
 
         if (!string.IsNullOrEmpty(token))
         {
